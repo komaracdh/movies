8d2d7b11f7966201d774bf914d88c28a21a4593a
Milos <komaracdh@gmail.com>
2019-05-28 18:36:53 +0200

    v2

---
+++ database/migrations/2019_05_28_162923_create_movies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('title');
+            $table->text('genre');
+            $table->text('director');
+            $table->text('storyline');
+            $table->date('date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
